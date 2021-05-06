fb6b1e8879f727a5c52932feae9e142a368ff779
hafis <hafis242@gmail.com>
2021-05-07 03:06:11 +0530

    2 way binding and routing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   aim="your perfect banking partner"
-  account="accno"
-  accno="";
+  account=""
+  accno="accno pls";
   pswd="";
   AccountDetails:any={
       1000: { acno: 1000, pswrd: "user1", balnce: 5000,uname:"user1" },
@@ -16,7 +17,7 @@ export class LoginComponent implements OnInit {
       1002: { acno: 1002, pswrd: "user3", balnce: 10000,uname:"user3" },
   
   }
-  constructor() { }
+  constructor(private router:Router) { }
   
 
   ngOnInit(): void {
@@ -33,14 +34,15 @@ export class LoginComponent implements OnInit {
     
 
   }
-  login(a:any,b:any){
+  login(){
     
-    var accno=a.value;
-    var pswd=b.value;
+    var accno=this.accno;
+    var pswd=this.pswd;
     let dataset=this.AccountDetails;
     if (accno in dataset){
       if (pswd==dataset[accno]["pswrd"]){
         alert("login successful")
+        this.router.navigateByUrl("dashboard")
       }
       else{
         alert("invalid password")
