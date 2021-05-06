22ccc0ca2195b1f668cc7bf714991baa44ed663e
hafis <hafis242@gmail.com>
2021-05-06 19:12:06 +0530

    event binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   aim="your perfect banking partner"
   account="accno"
+  accno="";
+  pswd="";
+  AccountDetails:any={
+      1000: { acno: 1000, pswrd: "user1", balnce: 5000,uname:"user1" },
+      1001: { acno: 1001, pswrd: "user2", balnce: 3000,uname:"user2" },
+      1002: { acno: 1002, pswrd: "user3", balnce: 10000,uname:"user3" },
+  
+  }
   constructor() { }
+  
 
   ngOnInit(): void {
   }
+  changeAcc(event:any){
+    this.accno=event.target.value
+    console.log(this.accno);
+    
+
+  }
+  changePswd(event:any){
+    this.pswd=event.target.value
+    console.log(this.pswd);
+    
+
+  }
+  login(){
+    alert("login clicked")
+    var accno=this.accno
+    var pswd=this.pswd
+    let dataset=this.AccountDetails
+    if (accno in dataset){
+      if (pswd==dataset[accno]["pswrd"]){
+        alert("login successful")
+      }
+      else{
+        alert("invalid password")
+      }
+    }
+    else{
+      alert("invalid account number")
+    }
+
+  }
+
+
 
 }
